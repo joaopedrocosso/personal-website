036382d14394f1b559d5f3b8949f56c55e41dcb1
JPMartins <jpmartins@nelogica.com.br>
2024-09-12 09:36:45 -0300

    style: Blog Article Y Axis Spacing

--- app/(blog)/blog/[slug]/page.tsx
+++ app/(blog)/blog/[slug]/page.tsx
@@ -28,7 +28,7 @@ export default async function BlogArticle({
     const data: fullBlog = await getData(params.slug)
     
     return(
-        <div className="px-10 flex flex-col items-center max-w-5xl mx-auto mt-8">
+        <div className="px-10 flex flex-col items-center max-w-5xl mx-auto my-28">
             <h1>
                 <span className="block text-base text-center text-[#4d4af9] font-semibold tracking-wide uppercase">
                     João Pedro Cosso - Blog
